8689d2226595752c7c905a697887b3e1896c6993
k1jeong <kjeong522@gmail.com>
2022-02-20 21:28:33 +0900

    카메라 권한 승인 및 웹뷰로 인식된 url 로드하기

--- QRCodeTutorial/MainViewController.swift
+++ QRCodeTutorial/MainViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 import WebKit
+import AVFoundation
+import QRCodeReader
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, QRCodeReaderViewControllerDelegate {
+        
     
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var qrcodeBtn: UIButton!
     
+    // QR코드리더 ViewController를 만든다
+    // Good practice: create the reader lazily to avoid cpu overload during the
+    // initialization and each time we need to scan a QRCode
+    lazy var readerVC: QRCodeReaderViewController = {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+            
+            // Configure the view controller (optional)
+            $0.showTorchButton        = false
+            $0.showSwitchCameraButton = false
+            $0.showCancelButton       = false
+            $0.showOverlayView        = true
+            $0.rectOfInterest         = CGRect(x: 0.2, y: 0.2, width: 0.6, height: 0.6)
+        }
+        
+        return QRCodeReaderViewController(builder: builder)
+    }()
     
+    //MARK: - 오버라이드 메소드
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,10 +50,44 @@ class MainViewController: UIViewController {
         
     }
 
-    
+    //MARK: - fileprivate 메소드
     @objc fileprivate func qrcodeReaderLaunch(){
         print("MainViewController - qrcodeReaderLaunch() called")
+        
+        // Retrieve the QRCode content
+        // By using the delegate pattern
+        readerVC.delegate = self
+
+        // Or by using the closure pattern
+        readerVC.completionBlock = { (result: QRCodeReaderResult?) in
+        print(result)
+            guard let scannedUrlString = result?.value else { return }
+            
+            print("scannedUrlString : \(scannedUrlString)")
+            let scannedUrl = URL(string: scannedUrlString)
+            self.webView.load(URLRequest(url: scannedUrl!))
+        }
+
+        // Presents the readerVC as modal form sheet
+        readerVC.modalPresentationStyle = .formSheet
+        
+        // 설정된 QR코드 뷰 컨트롤러를 화면에 보여준다.
+        present(readerVC, animated: true, completion: nil)
     }
 
+    //MARK: - QR code delegate 메소드
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+        reader.stopScanning()
+
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+        reader.stopScanning()
+
+        dismiss(animated: true, completion: nil)
+
+    }
+    
 }
 
